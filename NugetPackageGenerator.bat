ea02fd2d74e1e0805fe0377894b67f08c910f465
Atul Sagar <atul.sagar@ericsson.com>
2019-03-21 16:09:22 +0530

    added new files

---
+++ NugetPackageGenerator.bat
@@ -0,0 +1,51 @@
+ECHO OFF
+
+SET output_dir="C:\Personal\Projects\Engaze\local-nuget-server"
+
+SET version_suffix=std
+
+SET version_prefix=9000.1001.1
+
+SET project_dir="C:\Personal\Projects\Engaze\Core"
+
+SET curr_dir=%cd%"
+
+SET version="%version_prefix%-%version_suffix%"
+
+
+FOR /R %project_dir% %%G IN (*.csproj) DO (
+
+dotnet pack %%G -p:PackageVersion=%version% -p:TargetFrameworks=netstandard2.0 --no-build --output %output_dir% --configuration Release
+
+)
+
+ECHO %errorlevel%
+IF %errorlevel% NEQ 0 GOTO ERROR
+
+FOR %%G in ("%output_dir%\*.nupkg") DO (
+nuget add %%G -source %output_dir%
+)
+
+ECHO %errorlevel%
+IF %errorlevel% NEQ 0 ( GOTO ERROR )
+GOTO OK
+
+:ERROR
+ECHO ON
+ECHO "Program failed, please check this log file for errors ..."
+ECHO OFF
+GOTO END
+
+:OK
+ECHO ON
+ECHO "Program successful"
+ECHO OFF
+GOTO END
+
+:END
+CD %curr_dir%
+
+
+
+
+
